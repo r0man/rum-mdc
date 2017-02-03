8cd6b48b30ffac3678e4849f8b6c296c598c764d
r0man <roman@burningswell.com>
2017-02-03 14:32:50 +0100

    Version 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rum-mdc "0.0.3-SNAPSHOT"
+(defproject rum-mdc "0.0.3"
   :description "Material Design components for Rum"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
